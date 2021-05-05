7c658924bbf5bebe03be1bcad666ba38486e7b71
Geiler Elias Radillo Sarmiento <37160395+geilerelias@users.noreply.github.com>
2021-05-05 09:04:08 -0500

    agregando proyecto a la pagina pojects

--- public/16.js
+++ public/16.js
@@ -9,6 +9,8 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _images_COMFACESAR_INTERCULTURAL_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../images/COMFACESAR-INTERCULTURAL.jpg */ "./resources/images/COMFACESAR-INTERCULTURAL.jpg");
+/* harmony import */ var _images_COMFACESAR_INTERCULTURAL_jpg__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_images_COMFACESAR_INTERCULTURAL_jpg__WEBPACK_IMPORTED_MODULE_0__);
 //
 //
 //
@@ -36,9 +38,48 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
+      imageConfacesarIntercultural: _images_COMFACESAR_INTERCULTURAL_jpg__WEBPACK_IMPORTED_MODULE_0___default.a,
       links: [{
         text: "Home",
         disabled: false,
@@ -117,6 +158,129 @@ var render = function() {
           )
         ],
         1
+      ),
+      _vm._v(" "),
+      _c(
+        "main",
+        [
+          _c(
+            "v-sheet",
+            [
+              _c(
+                "v-container",
+                { staticClass: " py-4 py-lg-8" },
+                [
+                  _c(
+                    "v-row",
+                    { staticClass: "row" },
+                    [
+                      _c("v-col", { staticClass: "col-lg-6 col-12" }, [
+                        _c(
+                          "div",
+                          {
+                            staticClass:
+                              "text-uppercase font-weight-bold body-2 primary--text mb-2 mt-0 mt-xl-10"
+                          },
+                          [
+                            _vm._v(
+                              "\n                            Our projects\n                        "
+                            )
+                          ]
+                        ),
+                        _vm._v(" "),
+                        _c(
+                          "h2",
+                          {
+                            staticClass:
+                              "text-h5 title text-lg-h5 font-weight-black "
+                          },
+                          [
+                            _vm._v(
+                              "\n                            Comfacesar y entidad de EE. UU firman convenio de bilingüismo\n                        "
+                            )
+                          ]
+                        ),
+                        _vm._v(" "),
+                        _c(
+                          "div",
+                          {
+                            staticClass: "text-lg-h5  body-1 mt-5 text-justify"
+                          },
+                          [
+                            _vm._v(
+                              "\n                            Durante este año las instituciones educativas adscritas a la Caja de Compensación\n                            Familiar del Cesar, Comfacesar, contarán con diferentes beneficios para que los\n                            estudiantes y docentes que hacen parte de las mismas se fortalezcan en el manejo del\n                            inglés como segunda lengua.\n                        "
+                            )
+                          ]
+                        ),
+                        _vm._v(" "),
+                        _c(
+                          "div",
+                          { staticClass: "mt-4" },
+                          [
+                            _c(
+                              "v-btn",
+                              {
+                                attrs: {
+                                  outlined: "",
+                                  tile: "",
+                                  large: "",
+                                  color: "black",
+                                  href:
+                                    "https://elpilon.com.co/comfacesar-y-entidad-de-ee-uu-firman-convenio-de-bilinguismo/",
+                                  target: "_blank"
+                                }
+                              },
+                              [_vm._v(" Learn more")]
+                            ),
+                            _vm._v(" "),
+                            _c(
+                              "v-btn",
+                              {
+                                attrs: {
+                                  tile: "",
+                                  large: "",
+                                  color: "secondary",
+                                  href:
+                                    "http://www.comfacesar.com/articulo.aspx?idc=1098&fbclid=IwAR21UE59KuJEyLxfrmn6kPiXe-GPUau7p_bXR07wASUU3TiaVzNqYJhJoBE",
+                                  target: "_blank"
+                                }
+                              },
+                              [_vm._v(" And here")]
+                            )
+                          ],
+                          1
+                        )
+                      ]),
+                      _vm._v(" "),
+                      _c(
+                        "v-col",
+                        { staticClass: "col-lg-6 col-12" },
+                        [
+                          _c("v-img", {
+                            staticClass: "elevation-6",
+                            staticStyle: {
+                              "max-height": "480px",
+                              width: "5930px"
+                            },
+                            attrs: {
+                              cover: "",
+                              src: _vm.imageConfacesarIntercultural
+                            }
+                          })
+                        ],
+                        1
+                      )
+                    ],
+                    1
+                  )
+                ],
+                1
+              )
+            ],
+            1
+          )
+        ],
+        1
       )
     ],
     1
@@ -126,6 +290,17 @@ var staticRenderFns = []
 render._withStripped = true
 
 
+
+/***/ }),
+
+/***/ "./resources/images/COMFACESAR-INTERCULTURAL.jpg":
+/*!*******************************************************!*\
+  !*** ./resources/images/COMFACESAR-INTERCULTURAL.jpg ***!
+  \*******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/images/COMFACESAR-INTERCULTURAL.jpg?199bc5d2ecbe920655ba23200ad445df";
 
 /***/ }),
 
--- resources/js/components/Projects.vue
+++ resources/js/components/Projects.vue
@@ -22,13 +22,53 @@
                 </v-col>
             </v-row>
         </v-card>
+
+        <!--main content-->
+        <main>
+            <v-sheet>
+                <v-container class=" py-4 py-lg-8">
+                    <v-row class="row">
+                        <v-col class="col-lg-6 col-12">
+                            <div class="text-uppercase font-weight-bold body-2 primary--text mb-2 mt-0 mt-xl-10">
+                                Our projects
+                            </div>
+
+                            <h2 class="text-h5 title text-lg-h5 font-weight-black ">
+                                Comfacesar y entidad de EE. UU firman convenio de bilingüismo
+                            </h2>
+
+                            <div class="text-lg-h5  body-1 mt-5 text-justify">
+                                Durante este año las instituciones educativas adscritas a la Caja de Compensación
+                                Familiar del Cesar, Comfacesar, contarán con diferentes beneficios para que los
+                                estudiantes y docentes que hacen parte de las mismas se fortalezcan en el manejo del
+                                inglés como segunda lengua.
+                            </div>
+
+                            <div class="mt-4">
+                                <v-btn outlined tile large color="black"  href="https://elpilon.com.co/comfacesar-y-entidad-de-ee-uu-firman-convenio-de-bilinguismo/" target="_blank"> Learn more</v-btn>
+                                <v-btn  tile large color="secondary" href="http://www.comfacesar.com/articulo.aspx?idc=1098&fbclid=IwAR21UE59KuJEyLxfrmn6kPiXe-GPUau7p_bXR07wASUU3TiaVzNqYJhJoBE" target="_blank"> And here</v-btn>
+                            </div>
+                        </v-col>
+                        <v-col class="col-lg-6 col-12">
+                            <v-img cover style="max-height: 480px; width: 5930px" class="elevation-6"
+                                   :src="imageConfacesarIntercultural">
+                            </v-img>
+
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-sheet>
+        </main>
     </v-app>
 </template>
 
 <script>
+import imageConfacesarIntercultural from '../../images/COMFACESAR-INTERCULTURAL.jpg';
+
 export default {
     data() {
         return {
+            imageConfacesarIntercultural: imageConfacesarIntercultural,
             links: [
                 {
                     text: "Home",
@@ -40,7 +80,8 @@ export default {
                     disabled: true,
                     href: "/projects"
                 }
-            ]
+            ],
+
         };
     }
 };
